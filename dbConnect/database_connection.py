d6abe285d78535c7e69cb2401cdc2919c7fd1791
jm8084 <jm8084@rit.edu>
2021-10-19 17:49:04 -0400

    created .gitignore

--- dbConnect/database_connection.py
+++ dbConnect/database_connection.py
@@ -1,21 +1,24 @@
 import psycopg2
+import dbConnect.myLogin as info
 from sshtunnel import SSHTunnelForwarder
+
+dbName = 'p320_29'
 
 try:
 
     with SSHTunnelForwarder(
-    ('[1] starbug.cs.rit.edu', 22),
-    ssh_username="YOUR_CS_USERNAME",
-    ssh_password="YOUR_CS_PASSWORD",
+    ('starbug.cs.rit.edu', 22),
+    ssh_username=info.username,
+    ssh_password=info.password,
     remote_bind_address=('localhost', 5432)) as server:
 
         server.start()
         print("SSH tunnel established")
 
         params = {
-        'database': 'YOUR_CS_USERNAME',
-        'user': 'YOUR_CS_USERNAME',
-        'password': 'YOUR_CS_PASSWORD',
+        'database': dbName,
+        'user': info.username,
+        'password': info.password,
         'host': 'localhost',
         'port': server.local_bind_port
         }
