0f0bbf8164913be0ab9d539c1b98002ff2ceb35f
Andrew <ajp4312@g.rit.edu>
2021-10-28 20:09:19 -0400

    Initial commit of Request

--- application/commands/Request.py
+++ application/commands/Request.py
@@ -2,11 +2,29 @@
 class Request():
 
     def get_inputs(self):
-        pass
+        barcode = input("Barcode of tool to borrow: ")
+        username = input("Your username: ")
+        daterequired = input("Date the tool is required YYYY-MM-DD: ")
+        datereturned = input("Date to return the tool YYYY-MM-DD: ")
+
+        return barcode,username,daterequired,datereturned
 
     def execute(self, cur):
-        pass
+        values = self.get_inputs()
 
-    def toString(self) -> str:
-        return "none"
+        if values[0] == 'assign':
+            try:
+                result = self.cur.execute(f"INSERT INTO request(barcode, username,status,daterequired,datereturned) VALUES{values[0]}, {values[1]},{0},{values[2]},{values[3]} )")
+
+                # check for valid results
+                if result is None:
+                    return self.toString(result)
+                else:
+                    return self.toString(result)
+            except:
+                print('request failed!')
+
+
+    def toString(self,result) -> str:
+        return result
 
