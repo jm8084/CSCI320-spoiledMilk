a54094c9a7c1d17ebdb11ddd95c682a2d7eccfd6
Andrew <ajp4312@g.rit.edu>
2021-10-31 20:05:59 -0400

    Added other sorting to search, changed what was returned

--- application/commands/SearchTool.py
+++ application/commands/SearchTool.py
@@ -11,8 +11,13 @@ class SearchTool():
             search=input("Which category to search: ")
         else:
             print("Invalid search term")
-
-        return operation,search
+        order = input("Sort by 'name' or 'category', default is 'name': ")
+        if(order==""):
+            order='name'
+        direction=input("Sort 'asc'ending or 'desc'ending, default is 'asc'ending: ")
+        if(direction==""):
+            direction='asc'
+        return operation,search,order,direction
 
     def execute(self, cur, conn, user):
         values = self.get_inputs()
@@ -20,23 +25,27 @@ class SearchTool():
         try:
             if(values[0]=='barcode'):
             # print(f"INSERT INTO request(barcode, username,status,daterequired,datereturned) VALUES((SELECT barcode from tool WHERE tool.barcode={values[0]} and tool.sharable=1), '{user['username']}',{0},'{values[1]}','{values[2]}' )")
-                result = cur.execute(f"""SELECT * FROM tool 
+                command = f"""SELECT tool.barcode,tool.name,tool.description,tool.shareable,tool.purchasedate,tool.purchaseprice,c.categoryname FROM tool 
                     FULL OUTER JOIN tool_categories tc on tool.barcode = tc.barcode 
                     LEFT OUTER JOIN category c on tc.categoryid = c.categoryid
-                    WHERE tool.barcode={values[1]} ORDER BY tool.name asc;
-                    """)
+                    WHERE tool.barcode={values[1]}"""
             elif(values[0]=='name'):
-                result = cur.execute(f"""SELECT * FROM tool 
+                command = f"""SELECT tool.barcode,tool.name,tool.description,tool.shareable,tool.purchasedate,tool.purchaseprice,c.categoryname FROM tool 
                     FULL OUTER JOIN tool_categories tc on tool.barcode = tc.barcode 
                     LEFT OUTER JOIN category c on tc.categoryid = c.categoryid
-                    WHERE tool.name='{values[1]}' ORDER BY tool.name asc;
-                    """)
+                    WHERE tool.name='{values[1]}'"""
             elif(values[0]=='category'):
-                result = cur.execute(f"""SELECT * FROM tool 
+                command = f"""SELECT tool.barcode,tool.name,tool.description,tool.shareable,tool.purchasedate,tool.purchaseprice,c.categoryname FROM tool 
                 FULL OUTER JOIN tool_categories tc on tool.barcode = tc.barcode 
                 LEFT OUTER JOIN category c on tc.categoryid = c.categoryid
-                WHERE c.categoryname='{values[1]}' ORDER BY tool.name asc;
-                """)
+                WHERE c.categoryname='{values[1]}'"""
+
+            if(values[2]=='name'):
+                command += f" ORDER BY tool.name {values[3]};"
+            elif(values[2]=='category'):
+                command += f" ORDER BY c.categoryname {values[3]};"
+            # print(command)
+            result=cur.execute(command)
             conn.commit()
 
             rows = cur.fetchall()
