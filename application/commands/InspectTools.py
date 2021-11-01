15d853eb7c22655e429bd0c34fbbe600763e9087
EJ Martino <ejm8947@g.rit,edu>
2021-11-01 19:49:35 -0400

    Added more readable print to InspectTools

--- application/commands/InspectTools.py
+++ application/commands/InspectTools.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from datetime import date
 
 class InspectTools():
@@ -10,13 +9,10 @@ class InspectTools():
         return display
 
     def execute(self, cur, conn, user):
-        # TODO: Colouring or other overdue notifier
-        # Red - \033[91m
         display = self.get_inputs()
         try:
             if display == 'available' or display == 'all':
                 print('Available:')
-
                 # Select all shareable tools except when they're in the request table and request status = approved
                 cur.execute(f"SELECT barcode, name, description, purchasedate, purchaseprice "
                             f"FROM tool WHERE shareable = 1 "
@@ -25,39 +21,68 @@ class InspectTools():
                             f"FROM (tool t INNER JOIN request r on t.barcode = r.barcode)"
                             f"WHERE r.status = 1 "
                             f"ORDER BY name ASC")
+
+                # Print the header row
+                print(f"\033[1m | barcode \t| tool name \t| description \t| purchasedate \t| purchaseprice\033[0m")
+
                 for row in cur.fetchall():
-                    print(row)
+                    # Print all the rows that are returned from the executed SELECT statement
+                    print(f" | {row[0]}  \t\t| \t {row[1]}  \t| \t {row[2]}  \t|\t {row[3]}\t|\t {row[4]}")
+
             if display == 'lent' or display == 'all':
                 print('Lent:')
-                cur.execute(f"SELECT r.username, r.daterequired, r.datereturned, t.* FROM "
+                # Select needed data for any tool that is in the request table and the tool is owned by the user
+                # and the status of that request is 1 or 'approved'
+                cur.execute(f"SELECT r.username, r.daterequired, r.datereturned, "
+                            f"t.barcode, t.name, t.description, t.purchasedate, t.purchaseprice FROM "
                             f"((catalog c INNER JOIN request r on c.barcode = r.barcode) "
                             f"INNER JOIN tool t on r.barcode = t.barcode) "
                             f"WHERE c.username = '{user['username']}' AND r.status = 1 "
                             f"ORDER BY r.daterequired ASC")
+
+                # Print the header row
+                print(f"\033[1m | borrower \t| date required \t| return date \t| barcode \t| name \t"
+                      f"| description \t| purchase date \t| purchase price \033[0m")
+
                 for row in cur.fetchall():
+                    # Print all the rows that are returned from the executed SELECT statement
+                    # If they are overdue, color the line red
                     if row[2] < date.today() :
-                        print("\033[91m", end='')
-                        print(row)
-                        print("\033[0m",end='')
+                        print(f"\033[91m | {row[0]}  \t\t| \t {row[1]}  \t| \t {row[2]}  \t|\t {row[3]}\t|\t {row[4]} \t|\t"
+                              f" {row[5]} \t|\t {row[6]} \t|\t {row[7]} \033[0m")
                     else:
-                        print(row)
+                        print(f" | {row[0]}  \t\t| \t {row[1]}  \t| \t {row[2]}  \t|\t {row[3]}\t|\t {row[4]} \t|\t"
+                              f" {row[5]} \t|\t {row[6]} \t|\t {row[7]}")
+
             if display == 'borrowed' or display == 'all':
                 print('Borrowed:')
-                cur.execute(f"SELECT c.username, r.daterequired, r.datereturned, t.* FROM "
+                # Select needed data for any tool that is in the request table and the tool is requested by the user
+                # and the status of that request is 1 or 'approved'
+                cur.execute(f"SELECT c.username, r.daterequired, r.datereturned, "
+                            f"t.barcode, t.name, t.description, t.purchasedate, t.purchaseprice FROM "
                             f"((catalog c INNER JOIN request r on c.barcode = r.barcode) "
                             f"INNER JOIN tool t on r.barcode = t.barcode) "
                             f"WHERE r.username = '{user['username']}' AND r.status = 1 "
                             f"ORDER BY r.daterequired ASC")
+
+                # Print the header row
+                print(f"\033[1m | lender \t| date required \t| return date \t| barcode \t| name \t"
+                      f"| description \t| purchase date \t| purchase price \033[0m")
+
                 for row in cur.fetchall():
-                    if row[2] < date.today():
-                        print("\033[91m", end='')
-                        print(row)
-                        print("\033[0m", end='')
+                    # Print all the rows that are returned from the executed SELECT statement
+                    # If they are overdue, color the line red
+                    if row[2] < date.today() :
+                        print(f"\033[91m | {row[0]}  \t\t| \t {row[1]}  \t| \t {row[2]}  \t|\t {row[3]}\t|\t {row[4]} \t|\t"
+                              f" {row[5]} \t|\t {row[6]} \t|\t {row[7]}\033[0m")
                     else:
-                        print(row)
+                        print(f" | {row[0]}  \t\t| \t {row[1]}  \t| \t {row[2]}  \t|\t {row[3]}\t|\t {row[4]} \t|\t"
+                              f" {row[5]} \t|\t {row[6]} \t|\t {row[7]}")
+
         except Exception as e:
             print("Error getting records")
             print(e)
+
         finally:
             cur.close()
             return ''
