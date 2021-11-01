0981a7b042dd45b3f2f85d876c1844d99e93846b
Andrew <ajp4312@g.rit.edu>
2021-11-01 13:44:00 -0400

    Added returning tool, still need to fix return date

--- application/commands/ManageRequests.py
+++ application/commands/ManageRequests.py
@@ -1,5 +1,6 @@
 from application.commands import AcceptRequest, DenyRequest
 import psycopg2
+from datetime import date
 
 request_status=['pending','accepted','denied','completed']
 
@@ -22,10 +23,44 @@ class ManageRequests():
                 # view my requests
                 cur.execute("SELECT DISTINCT r.barcode, t.name, r.daterequired,r.datereturned, r.status FROM request r, tool t WHERE r.username = %s AND r.barcode = t.barcode", (user['username'],))
                 print("\033[1m | barcode \t| tool name \t| date required \t| date returned \t| status\033[0m")
-                for i in cur.fetchall():
-                    print(f" | {i[0]}  \t\t| \t {i[1]}  \t| \t {i[2]}  \t|\t {i[3]}\t|\t {request_status[i[4]]}\t|")
+                requests = cur.fetchall()
+                idx=0
+                for i in requests:
+                    print(f" {idx}| {i[0]}  \t\t| \t {i[1]}  \t| \t {i[2]}  \t|\t {i[3]}\t|\t {request_status[i[4]]}\t|")
+                    idx+=1
+                # return ''
 
-                return ''
+                action = input("Would you like to return a tool 'yes' or 'no', default 'no' : ")
+                if (action==""):
+                    action='no'
+                    return ""
+
+                if (action=='yes'):
+                    index = input("What is the index of the request for the tool you want to return: ")
+                    selected_request = requests[int(index)]
+                    print(selected_request)
+                    return_date = date.today().strftime("%Y-%m-%d")
+                    try:
+                        stat_result = cur.execute(
+                            f"UPDATE request SET status = 3 WHERE barcode = {selected_request[0]} AND username = '{user['username']}' AND daterequired = '{selected_request[2]}' AND datereturned = '{selected_request[3]}'")
+
+                        conn.commit()
+                        date_result = cur.execute(
+                            f"UPDATE request SET datereturned = '{return_date}' WHERE barcode = {selected_request[0]} AND username = '{user['username']}' AND daterequired = '{selected_request[3]}' AND status = 3")
+
+                        conn.commit()
+
+                        # print(stat_result)
+                        # print(date_result)
+                        # check for valid results
+                        if (stat_result is None) and (date_result is None):
+                            return '[+][Manage Request]Return Tool successful'
+
+                    except (psycopg2.DatabaseError) as e:
+
+                        conn.rollback()
+
+                        print(e)
 
             elif ((res == 'r') or (res == 'received')):
                 # view requests for my tools
@@ -43,18 +78,18 @@ class ManageRequests():
                 if action == 'a' or action == 'A' or action == 'accept' or action == 'Accept':
                         # accept
                     index = input("What is the index of the request you would like to accept: ")
-                    date = input("Change the due date of the tool, leave blank to not change YYYY-MM-DD: ")
+                    due_date = input("Change the due date of the tool, leave blank to not change YYYY-MM-DD: ")
                     try:
                         selected_request=requests[int(index)]
-                        if(date==""):
-                            date=selected_request[4]
+                        if(due_date==""):
+                            due_date=selected_request[4]
 
                         stat_result = cur.execute(
                              f"UPDATE request SET status = 1 WHERE barcode = {selected_request[0]} AND username = '{selected_request[2]}' AND daterequired = '{selected_request[3]}' AND datereturned = '{selected_request[4]}'")
 
                         conn.commit()
                         date_result = cur.execute(
-                             f"UPDATE request SET datereturned = '{date}' WHERE barcode = {selected_request[0]} AND username = '{selected_request[2]}' AND daterequired = '{selected_request[3]}' AND status = 1")
+                             f"UPDATE request SET datereturned = '{due_date}' WHERE barcode = {selected_request[0]} AND username = '{selected_request[2]}' AND daterequired = '{selected_request[3]}' AND status = 1")
 
                         conn.commit()
 
